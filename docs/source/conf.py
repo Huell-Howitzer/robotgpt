bf0b8b538c2220dcc4e6fa80a5967e79bca244a9
Ryan M Howell <rmhowell@protonmail.com>
2023-06-22 21:36:32 -0700

    Update Routes
    Has routes to reports and docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,12 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
+from datetime import datetime
+
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('.'))
 
 project = 'Robot GPT'
 copyright = '2023, Ryan M. Howell'
@@ -14,8 +20,6 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -24,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,37 +1,59 @@
 import os
 from flask import Flask, render_template, request, send_from_directory
+from flask import send_file
 
+app = Flask(__name__, template_folder='templates')
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/run', methods=['POST'])
+
+@app.route('/run', methods=['GET', 'POST'])
 def run():
-    # Handle the form submission and execute the engine here
-    # You can access form data using request.form['input_name']
-    # Perform the necessary logic to run the engine
-    # Return the results or redirect to a results page
+    if request.method == 'POST':
+        # Handle the form submission and execute the engine here
+        # You can access form data using request.form['input_name']
+        # Perform the necessary logic to run the engine
+        # Return the results or redirect to a results page
 
-    # Example code to run the engine and return a result
-    input_data = request.form['input_data']
-    # Perform the engine execution logic with the input data
-    result = "Program executed successfully!"
+        # Example code to run the engine and return a result
+        input_data = request.form['input_data']
+        # Perform the engine execution logic with the input data
+        result = "Program executed successfully!"
 
-    return render_template('results.html', result=result)
+        return render_template('results.html', result=result)
 
-@app.route('/reports/report.html')
+    # If the method is GET, render the form to input data
+    return render_template('run.html')
+
+
+@app.route('/reports')
 def serve_report():
+    reports_dir = os.path.join(app.root_path, 'reports')
     filename = "report.html"
-    reports_dir = os.path.abspath(os.path.join(os.getcwd(), '..', 'reports'))
-    return send_from_directory(reports_dir, filename)
+    file_path = os.path.join(reports_dir, filename)
 
+    print("File Path:", file_path)  # Debug statement
+
+    if os.path.exists(file_path):
+        return send_file(file_path)
+    else:
+        return "Report file not found."
+
+# Serve Sphinx documentation
+# Serve Sphinx documentation
+# Serve Sphinx documentation
+@app.route('/docs/<path:filename>')
+def serve_docs(filename):
+    docs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates', 'docs', 'html')
+    return send_from_directory(docs_dir, filename)
+
+@app.route('/about')
+def about():
+    return "About Page"
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
-
